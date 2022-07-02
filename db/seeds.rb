87bb0aa15d14440c7abd45ac416c07d76437d11b
hans bousquet lafond <hans.bl@icloud.com>
2022-07-02 15:38:26 +0100

    Wikinimous OK

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# require 'faker'
+
+puts "Seeding data…"
+
+10.times do
+  # Article.new(
+  #   title: Faker::ChuckNorris.fact,
+  #   content: Faker::ChuckNorris.fact).save
+  Article.create(
+      title: Faker::ChuckNorris.fact,
+      content: Faker::ChuckNorris.fact
+    )
+end
+
+puts "Data seeded!"
